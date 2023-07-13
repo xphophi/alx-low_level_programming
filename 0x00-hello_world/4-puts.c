8695d7639c6d77bbe409d71c76f721060fe05237
xphophi <phophie.pm@gmail.com>
2023-07-13 16:27:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
+
 /**
+
  * main - Entry point
+
  *
- * Return: 0 (Success)
+
+ * Return: Always 0 (Success)
+
  */
-int	main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		retuen	(0);
-	}
+
+int main(void)
+
+{
+
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
+
+}
